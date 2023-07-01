1fdd0b762a4b7eaafe4e81c1377e340c9ede2ddf
arctangent4 <rhyzoiddevelopment@gmail.com>
2023-07-01 12:40:58 -0400

    Fixed generator lag and overflow bugs

--- src/calc_32_bit.rs
+++ src/calc_32_bit.rs
@@ -1,60 +1,109 @@
 
 //MULTIPLY WITH CARRY 32 BIT GENERATOR
-pub fn mwc_calc_32_bit(init_x: u32, a: u32, base_val: u32, init_carry_c: u32, lag: u32, num_calcs: u32){
-    println!("mwc_32_bit function used.");
+
+pub fn mwc_calc_32_bit(init_x: u32, a: u32, base_val: u32, init_carry_c: u32, lag: u32, num_calcs: u32) {
+    println!("mwc_32_bit function used.\n");
+    // Converting types and Initialization
+    //Generator Parameters
+    let mut x_next: u32 = init_carry_c as u32;
+    let mut carry_c: u32 = init_x; 
+    let a: u32 = a as u32;
+
+    //Utility Variables
     let mut i = 0;
-    let mut output_list: Vec<f32>  = Vec::new();
+    let mut output_list: Vec<u32> = Vec::new();
+    let mut store_vals: Vec<u32> = Vec::new();
+
+    let mut interm_result: u32;
+    let mut lagged_index: u32;
     
-    let mut x_next: f32 = init_carry_c as f32;
-    let mut carry_c: f32 = init_x as f32;
-    let a: f32 = a as f32;
 
-    while i <= lag+num_calcs {
-        carry_c = a*x_next+carry_c;
-        carry_c = carry_c/(base_val as f32);
-        carry_c = carry_c.floor();
-        x_next = (a*x_next+carry_c) % (base_val as f32); 
+    // Pregenerating the lagging sequence and then returning desired number of 'random' numbers
+    while i <= lag + num_calcs {
+        // The carrying constant c is always the first thing to be manipulated.
+        // Note the first instantiation of x_next is not an actual term in the sequence, it is just the seed of the generator.
+        
+        interm_result = a.wrapping_mul(x_next); 
+        interm_result = interm_result.wrapping_add(carry_c);
+        carry_c = ((interm_result / (base_val))) as u32;
 
-        if i>lag{
+
+        // After c is found, a linear expression is evaluated and then mod base_val is taken(remember base_val = b-1).
+        // Similar to a Lehmer random number generator
+        interm_result= a.wrapping_mul(x_next);
+        interm_result = interm_result.wrapping_add(carry_c);
+        x_next = interm_result % (base_val);
+
+        //Logic to properly "lag" the generator.  store_vals is filled with lagged values during the pregeneration step.
+        store_vals.push(x_next);
+
+        if i == lag-1 {
+            x_next = store_vals[0];
+
+        } else if i >= lag{
+             //Once lagging is finished, the lagged values will be used to generate the output sequence.
             output_list.push(x_next);
+            lagged_index = i+1 - lag;
+            x_next = store_vals[lagged_index as usize];
         }
 
-        i+=1;
-      
+        i += 1;
     }
-    println!("{output_list:?}");
 
+    println!("{output_list:?}");
 }
 
 //COMPLEMENTARY MWC 32 BIT GENERATOR
-pub fn cmwc_calc_32_bit(init_x: u32, a: u32, base_val: u32, init_carry_c: u32, lag: u32, num_calcs: u32){
-    println!("cmwc_32_bit function used.");
+
+pub fn cmwc_calc_32_bit(init_x: u32, a: u32, base_val: u32, init_carry_c: u32, lag: u32, num_calcs: u32) {
+    println!("cmwc_64_bit function used.\n");
+    // Converting types and Initialization
+    //Generator Parameters
+    let mut x_next: u32 = init_carry_c as u32;
+    let mut carry_c: u32 = init_x; 
+    let a: u32 = a as u32;
+
+    //Utility Variables
     let mut i = 0;
-    let mut output_list: Vec<f32>  = Vec::new();
+    let mut output_list: Vec<u32> = Vec::new();
+    let mut store_vals: Vec<u32> = Vec::new();
+
+    let mut interm_result: u32;
+    let mut lagged_index: u32;
     
-    let mut x_next: f32 = init_carry_c as f32;
-    let mut carry_c: f32 = init_x as f32;
-    let a: f32 = a as f32;
+
+    // Pregenerating the lagging sequence and then returning desired number of 'random' numbers
+    while i <= lag + num_calcs {
+        // The carrying constant c is always the first thing to be manipulated.
+        // Note the first instantiation of x_next is not an actual term in the sequence, it is just the seed of the generator.
+        
+        interm_result = a.wrapping_mul(x_next); 
+        interm_result = interm_result.wrapping_add(carry_c);
+        carry_c = ((interm_result / (base_val))) as u32;
 
 
-    while i <= lag+num_calcs {
-        carry_c = a*x_next+carry_c;
-        carry_c = carry_c/(base_val as f32);
-        carry_c = carry_c.floor();
-        x_next = (a*x_next+carry_c) % (base_val as f32); 
-        //take the complement for the complementary carry generator
-        x_next = (base_val as f32) - x_next; 
-        if i>lag{
+        //The main difference between CMWC AND MWC: we take the complement of x_next relative to base_val
+        interm_result= a.wrapping_mul(x_next);
+        interm_result = interm_result.wrapping_add(carry_c);
+        interm_result = interm_result % (base_val);
+        x_next = base_val.wrapping_sub(interm_result);
+        
+
+       //Logic to properly "lag" the generator.  store_vals is filled with lagged values during the pregeneration step.
+        store_vals.push(x_next);
+
+        if i == lag-1 {
+            x_next = store_vals[0];
+
+        } else if i >= lag{
+            //Once lagging is finished, the lagged values will be used to generate the output sequence.
             output_list.push(x_next);
+            lagged_index = i+1 - lag;
+            x_next = store_vals[lagged_index as usize];
         }
-      
 
-        i+=1;
-      
+        i += 1;
     }
+
     println!("{output_list:?}");
-
-
-
-
 }
--- src/main.rs
+++ src/main.rs
@@ -16,39 +16,39 @@ fn main() {
     //These constants are needed to initialize the program. 
     //Capitalization for constants, lowercase for variables
     
-    const LAGGING_CONSTANT: u32 = 5;
-    const NUM_CALCS: u32 = 3;
+    const LAGGING_CONSTANT: u32 = 4096;
+    const NUM_CALCS: u32 = 5;
 
+    let init_c: u32 = 809430660;
+    let cmwc: bool = false;
+    let bit_size: u32;
+    let a: u32 = 18782;
 
-    const INIT_C: u32 = 809430660;
-  
+    let base_val: u64;
+
 
     
 
     // println!(" Welcome to the multiply-with-carry(MWC) pseudorandom generator. Please input the rng seed you want ");
     // io::stdin().read_line(&mut rng_seed).expect("failed to readline");
 
-    let cmwc: bool = false;
-    let bit_size: u32;
-    let base_val: u64;
-    let a: u32 = 18782;
 
-    
+    //target_pointer_width determines operating system size
     #[cfg(target_pointer_width = "32")]
     {
         //init_x is chosen to be randomly selected, just like in Marsaglia's implementation.
         //However, it can be manually changed with something like let init_x: u32 = 3.
         let rand_x: u32; 
         init_x = random();
-        println!("The randomly generated seed for the generator is {}", init_x);
+        println!("\nThe randomly generated seed for the generator is {} \n", init_x);
 
         bit_size = 32;
         base_val = u32::max_value() as u64 - 1;
 
         if cmwc {
-            cmwc_calc_32_bit(init_x,a, base_val, INIT_C, LAGGING_CONSTANT, NUM_CALCS);
+            cmwc_calc_32_bit(init_x,a, base_val, init_c, LAGGING_CONSTANT, NUM_CALCS);
         } else {
-            mwc_calc_32_bit(init_x,a, base_val, INIT_C, LAGGING_CONSTANT, NUM_CALCS);
+            output_list = mwc_calc_32_bit(init_x,a, base_val, init_c, LAGGING_CONSTANT, NUM_CALCS);
         }
     }
 
@@ -57,15 +57,15 @@ fn main() {
 
         let init_x: u64; 
         init_x = random();
-        println!("The randomly generated seed for the generator is {}", init_x);
+        println!("\nThe randomly generated seed for the generator is {}\n", init_x);
 
         bit_size = 64;
         base_val = u64::max_value() - 1;
 
         if cmwc {
-            cmwc_calc_64_bit(init_x,a, base_val, INIT_C, LAGGING_CONSTANT, NUM_CALCS);
+            cmwc_calc_64_bit(init_x,a, base_val, init_c, LAGGING_CONSTANT, NUM_CALCS);
         } else {
-            mwc_calc_64_bit(init_x,a, base_val, INIT_C, LAGGING_CONSTANT, NUM_CALCS);
+            mwc_calc_64_bit(init_x,a, base_val, init_c, LAGGING_CONSTANT, NUM_CALCS);
         }
     }
 
@@ -78,3 +78,4 @@ fn main() {
     } 
 
 }
+
